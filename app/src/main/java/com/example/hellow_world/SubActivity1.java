62d9f66abcf424205154c2ff441b714334a80451
Sumi Keisuke <keisukesumi0717@gmail.com>
2017-02-23 19:29:55 +0900

    new layout subactivity1

--- app/src/main/java/com/example/hellow_world/SubActivity1.java
+++ app/src/main/java/com/example/hellow_world/SubActivity1.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import static com.example.hellow_world.Define.*;
+
 /**
  * Created by ServoRobo on 2017/02/23.
  */
@@ -24,12 +26,18 @@ public class SubActivity1 extends Activity{
         // intentから指定キーの数字列を取得する
         intent.getIntArrayExtra("state");
 
-        if (state[0] == 1) {
-            findViewById(R.id.background1).setVisibility(View.INVISIBLE);
-            findViewById(R.id.background2).setVisibility(View.VISIBLE);
-        } else {
+        if (state[0] == 0) {
             findViewById(R.id.background1).setVisibility(View.VISIBLE);
             findViewById(R.id.background2).setVisibility(View.INVISIBLE);
+            findViewById(R.id.background3).setVisibility(View.INVISIBLE);
+        } else if(state[0]==1&&state[1]==0){
+            findViewById(R.id.background1).setVisibility(View.INVISIBLE);
+            findViewById(R.id.background2).setVisibility(View.VISIBLE);
+            findViewById(R.id.background3).setVisibility(View.INVISIBLE);
+        }else if(state[1]==1&&state[1]==1) {
+            findViewById(R.id.background1).setVisibility(View.INVISIBLE);
+            findViewById(R.id.background2).setVisibility(View.INVISIBLE);
+            findViewById(R.id.background3).setVisibility(View.VISIBLE);
         }
     }
 
@@ -45,14 +53,29 @@ public class SubActivity1 extends Activity{
 
 
         Log.d("Touchivemnt", "X:" + event.getX() + ",Y:" + event.getY());
-        if (flag==false){
-            if (1640<event.getY() && event.getY()<1950 && 400<event.getX() && event.getX()<830){
-                Intent intent = new Intent(getApplication(),SubActivity1.class);
-                intent.putExtra("state",state);
-                startActivityForResult(intent , 10011);
-                flag = true;
+        if(state[0]==1) {
+            if (flag == false) {
+                if (1640 < event.getY() && event.getY() < 1950 && 400 < event.getX() && event.getX() < 830) {
+                    Intent intent = new Intent(getApplication(), SubActivity1.class);
+                    intent.putExtra("state", state);
+                    startActivityForResult(intent, REQ_SUBSUB1);
+                    flag = true;
+                }else if(state[1]==1){
+                    state[0]=1;
+                    findViewById(R.id.background1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.background2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.background3).setVisibility(View.VISIBLE);
+                }
             }
         }
         return true;
+    }
+
+    protected void onActivityResult(int requestCode , int resultCode , Intent intent){
+        super.onActivityResult(requestCode,resultCode,intent);
+        if (requestCode == RESULT_OK && resultCode == REQ_SUBSUB1  && intent != null){
+            intent.getIntArrayExtra("state");
+            flag = false;
+        }
     }
 }
--- app/src/main/java/com/example/hellow_world/SubSubActivity1.java
+++ app/src/main/java/com/example/hellow_world/SubSubActivity1.java
@@ -1,8 +1,53 @@
 package com.example.hellow_world;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import static com.example.hellow_world.Define.*;
+
 /**
- * Created by primenote on 2017/02/23.
+ * Created by ServoRobo on 2017/02/23.
  */
 
-public class SubSubActivity1 {
+public class SubSubActivity1 extends Activity{
+    boolean flag=false;
+    int[] state = new int[3];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subactivity1);
+        // 現在のintentを取得する
+        Intent intent = getIntent();
+        // intentから指定キーの数字列を取得する
+        intent.getIntArrayExtra("state");
+
+        findViewById(R.id.background1).setVisibility(View.VISIBLE);
+    }
+
+    public void back(View v){
+        Intent intent = new Intent();
+        intent.putExtra("state",state);
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+
+        Log.d("Touchivemnt", "X:" + event.getX() + ",Y:" + event.getY());
+        if(state[0]==1&&state[1]==0) {
+            if (1640 < event.getY() && event.getY() < 1950 && 400 < event.getX() && event.getX() < 830) {
+                    state[1] = 1;
+            }
+        }
+        return true;
+    }
+
+
 }
