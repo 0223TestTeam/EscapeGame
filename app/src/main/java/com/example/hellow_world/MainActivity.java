5775cc4c7178ba7f6bbee81c27e8f73738864a68
Masahiro Matsuzaki <fivestar0737@gmail.com>
2017-02-23 13:46:25 +0900

    first commit

---
+++ app/src/main/java/com/example/hellow_world/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.hellow_world;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Screen1Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
